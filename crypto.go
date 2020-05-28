6962cfc1e99f3eef9db36a343e0abd9421edb35a
stanleychen <stanleychen2020123@gmail.com>
2020-05-28 18:31:22 +0800

    add crypto
    Signed-off-by: stanleychen <stanleychen2020123@gmail.com>

---
+++ crypto.go
@@ -0,0 +1,164 @@
+/**
+ * @author:       Stanley Chan
+ * @description:  description
+ * @date:         2019-04-22
+ */
+package utils
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/pem"
+	"errors"
+	"log"
+)
+
+func Base64Encode(src []byte) []byte {
+	buf := make([]byte, base64.StdEncoding.EncodedLen(len(src)))
+	base64.StdEncoding.Encode(buf, src)
+	return buf
+}
+
+func Base64Decode(src []byte) ([]byte, error) {
+	dbuf := make([]byte, base64.StdEncoding.DecodedLen(len(src)))
+	n, err := base64.StdEncoding.Decode(dbuf, src)
+	return dbuf[:n], err
+}
+
+func RsaEncrypt(ori []byte, pubkey []byte) ([]byte, error) {
+	block, _ := pem.Decode(pubkey)
+	if block == nil {
+		return nil, errors.New("public key error")
+	}
+
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	return rsa.EncryptPKCS1v15(rand.Reader, pub, ori)
+}
+
+func RsaDecrypt(cipher []byte, priKey []byte) ([]byte, error) {
+	block, _ := pem.Decode(priKey)
+	if block == nil {
+		return nil, errors.New("private key error")
+	}
+
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return rsa.DecryptPKCS1v15(rand.Reader, priv, cipher)
+}
+
+func Md5Encrypt(data []byte) string {
+	md5Ctx := md5.New()
+	md5Ctx.Write(data)
+	cipher := md5Ctx.Sum(nil)
+	encryptedData := hex.EncodeToString(cipher)
+	return encryptedData
+}
+
+/**
+ * Generate RSA private key and public key
+ */
+func genRsaKey(bits int) ([]byte, []byte, error) {
+	priKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	derStream := x509.MarshalPKCS1PrivateKey(priKey)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: derStream,
+	}
+
+	prkBuf := new(bytes.Buffer)
+	err = pem.Encode(prkBuf, block)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pubKey := &priKey.PublicKey
+	derPix, err := x509.MarshalPKIXPublicKey(pubKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	block = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: derPix,
+	}
+
+	pbkBuf := new(bytes.Buffer)
+	err = pem.Encode(pbkBuf, block)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return prkBuf.Bytes(), pbkBuf.Bytes(), nil
+}
+
+func AesEncrypt(key, message string) (string, error) {
+	hash := md5.New()
+	hash.Write([]byte(key))
+	keyData := hash.Sum(nil)
+
+	block, err := aes.NewCipher(keyData)
+	if err != nil {
+		return "", err
+	}
+
+	iv := []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+
+	enc := cipher.NewCBCEncrypter(block, iv)
+	content := PKCS5Padding([]byte(message), block.BlockSize())
+	crypted := make([]byte, len(content))
+	enc.CryptBlocks(crypted, content)
+	return base64.StdEncoding.EncodeToString(crypted), nil
+}
+
+func AesDecrypt(key, message string) (string, error) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("panic message is ", message, "err is ", err)
+		}
+	}()
+	hash := md5.New()
+	hash.Write([]byte(key))
+	keyData := hash.Sum(nil)
+
+	block, err := aes.NewCipher(keyData)
+	if err != nil {
+		return "", nil
+	}
+
+	iv := []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
+
+	messageData, _ := base64.StdEncoding.DecodeString(message)
+	dec := cipher.NewCBCDecrypter(block, iv)
+	decrypted := make([]byte, len(messageData))
+	dec.CryptBlocks(decrypted, messageData)
+	return string(PKCS5Unpadding(decrypted)), nil
+}
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5Unpadding(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
+}
